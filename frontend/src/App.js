d864e39a00be39b216a9678c858f40564544c1d9
Joao Pedro <joao.ricardo88@gmail.com>
2020-11-24 00:39:27 +0000

    react added

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Books from './components/books';
+
+function App() {
+  return (
+    <div className="App">
+      <Books />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/books.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+export class books extends Component {
+
+    state={
+       books: [],
+       isLoaded: false 
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:8000/wp-json/wp/v2/books')
+            .then(res => this.setState({
+                books: res.data,
+                isLoaded:true
+            }))
+            .catch(err => console.log(err));
+    }
+
+    render() {
+        const { books, isLoaded } = this.state;
+        console.log(books);
+        if (isLoaded) {
+             return (
+            <div>
+               
+                { books.map(book => {
+                    <h4>{book.id.rendered}</h4>
+                })}
+            </div>
+        )
+        }
+       
+        return <h3>Loading...</h3>
+    }
+}
+
+export default books
